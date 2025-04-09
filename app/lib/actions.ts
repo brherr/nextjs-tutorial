2675a096e501a0672944205daf928faf48112b02
brherr <herrbrad@yahoo.com>
2025-04-09 13:50:20 -0400

    ch 13 - error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,10 +27,14 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
-	INSERT INTO invoices (customer_id, amount, status, date )
-	VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-	`;
+  try {
+    await sql`
+		INSERT INTO invoices (customer_id, amount, status, date )
+		VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+		`;
+  } catch (error) {
+    console.error(error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -44,10 +48,14 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
 		UPDATE invoices
 		SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
 		WHERE id = ${id}`;
+  } catch (error) {
+    console.error(error);
+  }
 
   revalidatePath(`/dashboard/invoices`);
   redirect("/dashboard/invoices");
@@ -55,8 +63,9 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 export async function deleteInvoice(id: string) {
   await sql`
-		DELETE FROM invoices
-		WHERE id = ${id}
-	`;
+			DELETE FROM invoices
+			WHERE id = ${id}
+		`;
+
   revalidatePath(`/dashboard/invoices`);
 }
